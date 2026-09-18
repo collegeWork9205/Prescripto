c88bed37d58d5ebc6f1525f0e631be5248202246
collegeWork9205 <aycollege53230203@gmail.com>
2026-09-18 22:14:53 +0530

    Update Sidebar component for responsive design

--- Admin/src/Component/Sidebar.jsx
+++ Admin/src/Component/Sidebar.jsx
@@ -11,21 +11,21 @@ const Sidebar = () => {
         <div className='min-h-screen bg-white border-r'>
             {
                 aToken && <ul className='text-[#515151] mt-5'>
-                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/admin-dashboard'}>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 max-sm:min-w-15 max-lg:min-w-25 xl:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/admin-dashboard'}>
                         <img src={assets.home_icon} alt="" />
-                        <p className='text-xs sm:text-sm'>Dashboard</p>
+                        <p className='text-xs sm:text-sm max-lg:hidden'>Dashboard</p>
                     </NavLink>
-                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/all-appointments'}>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 max-sm:min-w-15 max-lg:min-w-25 xl:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/all-appointments'}>
                         <img src={assets.appointment_icon} alt="" />
-                        <p className='text-xs sm:text-sm'>Appointments</p>
+                        <p className='text-xs sm:text-sm max-lg:hidden'>Appointments</p>
                     </NavLink>
-                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/add-doctor'}>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 max-sm:min-w-15 max-lg:min-w-25 xl:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/add-doctor'}>
                         <img src={assets.add_icon} alt="" />
-                        <p className='text-xs sm:text-sm'>Add Doctor</p>
+                        <p className='text-xs sm:text-sm max-lg:hidden'>Add Doctor</p>
                     </NavLink>
-                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/doctor-list'}>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-2 md:px-9 min-w-32 max-sm:min-w-15 max-lg:min-w-25 xl:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#536fff]' : ''}`} to={'/doctor-list'}>
                         <img src={assets.people_icon} alt="" />
-                        <p className='text-xs sm:text-sm'>Doctors List</p>
+                        <p className='text-xs sm:text-sm max-lg:hidden'>Doctors List</p>
                     </NavLink>
                 </ul>
             }
